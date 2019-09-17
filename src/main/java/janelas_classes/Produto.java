c2ff41b092a9d916fccb94876df6fdb09b7eb846
elton-tanaka <elton.tanaka@univem.edu.br>
2019-09-17 20:02:06 -0300

    commit 2

---
+++ src/main/java/janelas_classes/Produto.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package janelas_classes;
+
+/**
+ *
+ * @author EltonSSD
+ */
+public class Produto 
+{
+    private String nome;
+    private float preco;
+}
